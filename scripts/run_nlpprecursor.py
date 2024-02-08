70619e29330f5be8c55bc69e2185bfde0ef41ce2
Taylor Reiter <taylorreiter@gmail.com>
2024-02-08 19:07:35 +0000

    add a try function to avoid RuntimeError: cuDNN error: CUDNN_STATUS_EXECUTION_FAILED

--- scripts/run_nlpprecursor.py
+++ scripts/run_nlpprecursor.py
@@ -4,9 +4,40 @@ from nlpprecursor.annotation.data import DatasetGenerator as ADG
 from pathlib import Path
 import nlpprecursor
 from Bio import SeqIO
+import time
 
 # This allows for backwards compatibility of the pickled models.
 sys.modules["protai"] = nlpprecursor
+
+def robust_predict(predict_function, *args, max_attempts=2, sleep_time=1):
+    """
+    Attempts to call the predict function up to a maximum number of attempts.
+    On first attempt to call this function on a GPU, the function produces a RuntimeError: cuDNN error: CUDNN_STATUS_EXECUTION_FAILED.
+    Second execution succeeds.
+    Execution fails on the @classmethod predict(), on the line `predictions = model(tokens)[0]`.
+    Args:
+        predict_function: The prediction function to call. In this case, CDG.predict, since it's the first predict function called in the script.
+        *args: Arguments to pass to the prediction function.
+        max_attempts (int): Maximum number of attempts to make.
+        sleep_time (int or float): Time to wait between attempts, in seconds.
+    Returns:
+        The result of the prediction function if successful.
+    Raises:
+        Exception: Re-raises the last exception if all attempts fail.
+    """
+    for attempt in range(max_attempts):
+        try:
+            # Try to make the prediction
+            return predict_function(*args)
+        except Exception as e:
+            print(f"Attempt {attempt + 1} failed with error: {e}")
+            if attempt + 1 < max_attempts:
+                print(f"Retrying in {sleep_time} seconds...")
+                time.sleep(sleep_time)  # Wait a bit before retrying
+            else:
+                print("All attempts failed. Raising the last exception.")
+                raise  # Re-raise the last exception if out of attempts
+
 
 def main(models_dir, multifasta_file, output_tsv):
     models_dir = Path(models_dir)  
@@ -26,7 +57,10 @@ def main(models_dir, multifasta_file, output_tsv):
         sequences.append({"sequence": str(record.seq), "name": record.id})
 
     # Predict class and cleavage for each sequence
-    class_predictions = CDG.predict(class_model_path, class_vocab_path, sequences)
+    try:
+        class_predictions = robust_predict(CDG.predict, class_model_path, class_vocab_path, sequences)
+    except Exception as final_error:
+        print(f"Failed to predict class after several attempts: {final_error}")
     cleavage_predictions = ADG.predict(annot_model_path, annot_vocab_path, sequences)
 
     # The output of nlpprecursor predictions are in JSON format.
