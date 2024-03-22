98fbad774e92c53a8bd1ec4be00df2e374561de2
Taylor Reiter <taylorreiter@gmail.com>
2024-03-22 14:51:59 +0000

    add filter for alignment length

--- scripts/filter_transcript_uniref50_hits.py
+++ scripts/filter_transcript_uniref50_hits.py
@@ -8,12 +8,12 @@ def filter_blast_results(input_file, output_blast, output_names):
     Filters DIAMOND BLAST results to identify and retain hits that are likely to correspond to long
     protein-coding sequences, based on predefined criteria of e-value and sequence length (slen).
 
-    This function filters out BLAST hits with an e-value less than 1e-10 and a subject (hit)
-    sequence length (slen) of less than 100 amino acids.
-    These thresholds are chosen to select transcripts likely hitting long protein coding sequences
-    in databases like UniRef50, thus suggesting that these transcripts are less likely to encode
-    short open reading frame (sORF) peptides because they are likely fragments of true long
-    protein-coding genes.
+    This function filters out BLAST hits with an e-value less than 1e-10, a subject (hit)
+    sequence length (slen) of less than 100 amino acids, and an alignment length of greater than 100
+    amino acids. These thresholds are chosen to select transcripts likely hitting long protein
+    coding sequences in databases like UniRef50, thus suggesting that these transcripts are less
+    likely to encode short open reading frame (sORF) peptides because they are likely fragments of
+    true long protein-coding genes.
 
     Parameters:
     - input_file (str): Path to the input file containing DIAMOND BLAST results.
@@ -25,7 +25,9 @@ def filter_blast_results(input_file, output_blast, output_names):
     and the second output file contains only the qseqid of the retained hits.
     """
     blast_hits = pd.read_csv(input_file, sep="\t")
-    filtered_blast_hits = blast_hits[(blast_hits["evalue"] < 1e-10) & (blast_hits["slen"] > 100)]
+    filtered_blast_hits = blast_hits[
+        (blast_hits["evalue"] < 1e-10) & (blast_hits["slen"] > 100) & (blast_hits["length"] > 100)
+    ]
     filtered_sorted_blast_hits = filtered_blast_hits.sort_values(
         by=["qseqid", "bitscore", "evalue"], ascending=[True, False, True]
     )
