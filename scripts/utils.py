f878cf713b3cf65daa773c9d0f98768f9e4e9f21
Taylor Reiter <taylorreiter@gmail.com>
2024-06-24 11:10:15 -0400

    woops rm commented out code

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,11 +1,6 @@
 from Bio import SeqIO
 from Bio.Seq import Seq
 
-# def verify_translation(nucleotide_seq, amino_acid_seq, to_stop):
-#    """Verify that a nucleotide sequence translates correctly to its amino acid sequence."""
-#    translated_seq = Seq(str(nucleotide_seq)).translate(to_stop=to_stop)
-#    return str(translated_seq) == str(amino_acid_seq)
-
 
 def verify_translation(nucleotide_seq, amino_acid_seq, to_stop, allow_wildcard_x=False):
     """
